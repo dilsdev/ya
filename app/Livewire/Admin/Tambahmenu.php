93b5f470e728a2b1f56d57858da40c06881b7186
abdil <abdil150507@gmail.com>
2024-06-29 13:29:09 +0700

    menurunkan resolusi gambar

--- app/Livewire/Admin/Tambahmenu.php
+++ app/Livewire/Admin/Tambahmenu.php
@@ -25,13 +25,38 @@ class Tambahmenu extends Component
         $this->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif',
         ]);
-        $this->image->storeAs('public/menu', $this->image->hashName());
+        $imagePath = $this->image->getRealPath();
+        $imageName = $this->image->hashName();
+        $img = imagecreatefromstring(file_get_contents($imagePath));
+        if ($img === false) {
+            session()->flash('message', 'Gagal memproses gambar.');
+            return;
+        }
+        $width = imagesx($img);
+        $height = imagesy($img);
+
+        $newWidth = 260;
+        $newHeight = 380;
+
+        if ($width > $height) {
+            $newHeight = intval($height * $newWidth / $width);
+        } else {
+            $newWidth = intval($width * $newHeight / $height);
+        }
+        $newImg = imagecreatetruecolor($newWidth, $newHeight);
+        imagecopyresampled($newImg, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+        $savePath = storage_path('app/public/menu' . $imageName);
+        imagejpeg($newImg, $savePath, 60);
+
+        imagedestroy($img);
+        imagedestroy($newImg);
 
         ModelMenu::create([
             'nama' => $this->nama,
             'deskripsi' => $this->deskripsi,
             'harga' => $this->harga,
-            'image' => $this->image->hashName(),
+            'image' => $imageName,
             'status' => $this->status,
             'jenis' => $this->jenis
         ]);
